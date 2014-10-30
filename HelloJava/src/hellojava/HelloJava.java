b01cabae0904cfdd822aadfec0b60b59f617682c
Md. Mahadi Hasan Nahid <nahid@student.sust.edu>
2014-10-29 17:24:12 -0700

    Added Hello World Project

---
+++ HelloJava/src/hellojava/HelloJava.java
@@ -0,0 +1,17 @@
+
+package hellojava;
+
+/**
+ *
+ * @author MdMahadiHasan
+ */
+public class HelloJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
